d5e1e1c73ecb19865da14931e3e51d6c6c2c2aa4
KaveethaHerath <139349662+KaveethaHerath@users.noreply.github.com>
2024-02-13 00:24:38 +0530

    notes

---
+++ src/com/raven/notes/note.java
@@ -0,0 +1,19 @@
+
+package com.raven.notes;
+
+import java.util.Date;
+
+public class note {
+    public String Note_name;
+    //blic String Task_description;
+    public String Note_date;
+    
+    
+    public note(String Note_name,String Note_date){
+        
+        this.Note_name=Note_name;
+        //this.Task_description=Task_description;
+        this.Note_date=Note_date;
+    
+    }
+}
